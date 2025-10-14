56449d2a1f98dc40723391a1fae1257ec6b89149
Chris Kennedy <chrisken@gmail.com>
2025-10-13 21:24:57 -0400

    fix coverage: add unit test and adjust codecov config

--- src/lib.rs
+++ src/lib.rs
@@ -9,3 +9,16 @@ impl zed::Extension for QuartoExtension {
 }
 
 zed::register_extension!(QuartoExtension);
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use zed::Extension;
+
+    #[test]
+    fn test_extension_can_be_created() {
+        // Verify the extension can be instantiated
+        let _extension = QuartoExtension::new();
+        // Extension creation should succeed without panicking
+    }
+}
